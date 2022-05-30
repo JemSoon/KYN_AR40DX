bab2f8015e472497eb72addadc2bfe6fc4a7aadc
잼순 김 <highyeana@naver.com>
2022-05-30 20:39:30 +0900

    함수명 편하게 보기위해 선생님과 동일화

--- KYN_AR40DX/GameEngineApp/AppMain.cpp
+++ KYN_AR40DX/GameEngineApp/AppMain.cpp
@@ -4,6 +4,7 @@
 
 #include <GameEngineBase/GameEngineDebug.h>
 #include <GameEngineBase/GameEngineDirectory.h>
+#include <GameEngineBase/GameEngineFile.h>
 #include <GameEngineBase/GameEngineSound.h>
 
 
@@ -17,10 +18,24 @@ int main()
 		return 0;
 	}
 
+	Dir.Move("Resources");
+	Dir.Move("Sound");
+
+	std::vector<GameEngineFile> Files = Dir.GetAllFile();
+
+	// Range for
+	// Files 특정한 오퍼레이터가 되어있으면 
+	for (GameEngineFile& File : Files)
+	{
+		GameEngineSound::LoadRessource(File);
+		// GameEngineSound::LoadRessource("BGM.MP3");
+	}
+
+	GameEngineSound::SoundPlayOneShot("BGM.mp3");
 
 	while (true)
 	{
-
+		GameEngineSound::Update();
 	}
 
 	return 0;
